b84fa194178426c7f07a54bd56cf9c571acbda03
Will Webberley <willwebberley@gmail.com>
2016-10-05 22:19:00 +0100

    removed some un-needed code. TODO: modify existing code to read blog posts from file

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, url_for, render_template, request, session, escape, red
 import os,datetime, time, math
 from blog import *
 import json
-from tools import *
 
 app = Flask(__name__)
 admin_password = os.environ.get('FLYINGSPARX_PASSWORD')
@@ -14,13 +13,6 @@ app.secret_key = os.environ.get('FLYINGSPARX_KEY')
 def transfer():
     transferPosts()
     return "Done"
-
-# On each request, check if requester is mobile device.
-# If so, set new request field to True so views can pass this info
-# to the templates.
-@app.before_request
-def check_mobile():
-    request.mobile = detectMobile(request)
 
 # flyingsparx.net/ 
 @app.route('/')
@@ -37,7 +29,7 @@ def cv():
 @app.route('/blog')
 def blog():
     postList = getPosts()
-    return render_template('blog.html', posts = postList, mobile = request.mobile, single = False)
+    return render_template('blog.html', posts = postList, single = False)
 
 
 # flyingsparx.net/post/year/month/day
@@ -48,7 +40,7 @@ def post(year, month, day):
     postList = []
     if not post == None:
         postList.append(post)
-    return render_template('blog.html', posts = postList, mobile = request.mobile, single = True)
+    return render_template('blog.html', posts = postList, single = True)
 
 # flyingsparx.net/post/year/month/day/title
 # Get the post posted on this day and pass to template for rendering.
@@ -59,143 +51,38 @@ def postByTitle(year, month, day, title):
     postList = []
     if not post == None:
         postList.append(post)
-    return render_template('blog.html', posts = postList, mobile = request.mobile, single = True)
+    return render_template('blog.html', posts = postList, single = True)
 
 # flyingsparx.net/contact
 @app.route('/contact/')
 def contact():
-        return render_template('contact.html', mobile = request.mobile)
+        return render_template('contact.html', )
 
 # flyingsparx.net/research
 @app.route('/research/')
 def research():
-        return render_template('research.html', mobile = request.mobile)
+        return render_template('research.html')
 
 # flyingsparx.net/output
 @app.route('/output/')
 def output():
-        return render_template('output.html', mobile = request.mobile)
+        return render_template('output.html')
 
 # flyingsparx.net/notes
 @app.route('/notes/')
 def notes():
-        return render_template('notes.html', mobile = request.mobile)
+        return render_template('notes.html')
 
 
 # flyingsparx.net/project
 @app.route('/project/')
 def project():
-        return render_template('project.html', mobile = request.mobile)
+        return render_template('project.html')
 
 # flyingsparx.net/teaching
 @app.route('/teaching/')
 def teaching():
-        return render_template('teaching.html', mobile = request.mobile)
-
-@app.route('/photos/')
-def photos():
-    photos = []
-    for root, _, files in os.walk('static/photos/thumbs/'):
-        for f in files:
-            photos.append(str(f))
-    return render_template('photos.html', mobile = request.mobile, photos = photos)
-
-
-# flyingsparx.net/logout
-# Logout of admin interface - pop cookie from session list
-@app.route('/logout/')
-def logout():
-    session.pop('id')
-    return redirect(url_for('show'))
-
-# flyingspsarx.net/blog
-# Redirect to the blog list page
-@app.route('/manage/')
-def manage():
-    return redirect(url_for('show'))
-
-# flyingsparx.net/blog/new
-# If logged in and GET, show form for writing a new blog post.
-# if POST, get data from form and write to db
-@app.route('/blog/new/', methods=['GET', 'POST'])
-def new():
-    if not 'id' in session:
-        return redirect(url_for('show'))
-    if not session['id'] == access_token:
-        return redirect(url_for('show'))
-    if request.method == 'GET':
-        return '''<form method="POST" action="/blog/new/">
-                    <input type="text" placeholder="title" name="title" /><hr /><textarea name="text" placeholder="text"></textarea><hr />
-                    <input type="text" placeholder="day" name="day" /><input type="text" placeholder="month" name="month" />
-                    <input type="text" placeholder="year" name="year" />
-                    <input type="submit" value="submit" /></form>'''
-    if request.method == 'POST':
-        title = request.form['title']
-        text = request.form['text']
-        day = str(request.form['day'])
-        month = str(request.form['month'])
-        year = str(request.form['year'])
-        newPost(title, text, day, month, year)
-    return redirect(url_for('show'))
-
-# flyingsparx.net/blog/show
-# If logged in, show blog list with options for creating new blogs, logging out and editing existing posts
-# If not logged in, show login form
-@app.route('/blog/show/', methods=['GET', 'POST'])
-def show():
-    if request.method == 'POST':
-        if request.form['password'] == admin_password:
-            session['id'] = access_token
-        return redirect(url_for('show'))
-    if not 'id' in session:
-        return 'Password: <form method="post" action="/blog/show/"><input type="password" name="password" /><input type="submit" value="submit" /></form>'
-    if not session['id'] == access_token:
-        return 'Password: <form method="post" action="/blog/show/"><input type="password" name="password" /><input type="submit" value="submit" /></form>'
-    posts = getPosts()
-    ret = "<a href='/blog/new/'>Create a new post</a> - <a href='/logout/'>Logout</a><hr />"
-    for post in posts:
-        ret=ret+"<h1>"+post.title+" ("+str(post.day)+"/"+str(post.month)+"/"+str(post.year)+")</h1>"
-        ret=ret+'<a href="/blog/edit/'+str(post.id)+'">edit</a>'
-    return ret
-
-#flyingsparx.net/blog/edit/blog_id
-# If logged in and GET, show 'new post' form with fields completed with this post's details
-# if POST, get data from fields and update the blog database
-@app.route('/blog/edit/<blog_id>', methods=['GET', 'POST'])
-def edit(blog_id):
-    if not 'id' in session:
-        return redirect(url_for('show'))
-    if not session['id'] == access_token:
-        return redirect(url_for('show'))
-    if request.method == 'GET':
-        post = getPostById(blog_id)
-        return '''<form method="POST" action="/blog/edit/'''+str(blog_id)+'''">
-                <input type="text" placeholder="title" name="title" value="'''+str(post.title)+'''"/><hr /><textarea name="text" placeholder="text">'''+post.content+'''</textarea><hr />
-                <input type="text" placeholder="day" name="day" value="'''+str(post.day)+'''"/><input type="text" placeholder="month" name="month" value="'''+str(post.month)+'''"/>
-                <input type="text" placeholder="year" name="year" value="'''+str(post.year)+'''"/>
-                <hr /><input type="submit" value="submit" /></form><hr /><a href="/blog/delete/'''+str(blog_id)+'''">Delete this post</a>'''
-    if request.method == 'POST':
-        updatePost(blog_id, request.form['title'], request.form['text'], str(request.form['day']), str(request.form['month']), str(request.form['year']))
-        return redirect(url_for('show'))
-
-# flyingsparx.net/blog/delete/blog_id
-@app.route('/blog/delete/<blog_id>')
-def delete(blog_id):
-    if not 'id' in session:
-        return redirect(url_for('show'))
-    if not session['id'] == access_token:
-        return redirect(url_for('show'))
-    deletePost(blog_id)
-    return redirect(url_for('show'))
-
-# flyingsparx.net/blog/get
-# JSON endpoint for returning blog data
-# Currently used for embedding blog in willwebberley.net
-@app.route('/blog/get')
-@crossdomain(origin='*')
-def getBlog():
-    posts = getPostsAsJSON()
-    return posts
+        return render_template('teaching.html')
 
 # Main code
 if __name__ == '__main__':
--- blog.py
+++ blog.py
@@ -1,37 +1,20 @@
 # Methods for blog support.
-# Includes functionality for creating, editing, listing, deleting blog posts, etc.
 
 import datetime, time, math, json
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('sqlite:///static/data/posts.db')
-Base = declarative_base()
-Session = sessionmaker(bind=engine)
-sess = Session()
-disallowed_in_url = ["'","(",")","?","%","&","!","@","+",":"]
-
-class Post(Base):
-    __tablename__ = 'posts'
-
-    id = Column(Integer, primary_key=True)
+class Post:
     title = Column(String)
     content = Column(String)
-    date = Column(Integer)
     day = Column(String)
     month = Column(String)
     year = Column(String)
 
-    def __init__(self,id,title,content,date,day,month,year):
-        self.id = id
+    def __init__(self,title,content,day,month,year):
         self.title = title
         self.content = content
-        self.date = date
         self.day = day
         self.month = month
         self.year = year
-Base.metadata.create_all(engine)
 
 # Process the provided post to add a url_title field
 # this field should be used as part of the 
@@ -68,41 +51,3 @@ def getPostByDateAndTitle(year, month, day, title):
         post = processPost(post)
         if post.url_title.lower() == title.lower():
             return post
-
-# Return all posts in JSON format (for external feeds)
-def getPostsAsJSON():
-    postList=[]
-    for post in sess.query(Post).order_by(Post.date.desc()):    
-        date = str(post.day)+"/"+str(post.month)+"/"+str(post.year)
-        url = "http://flyingsparx.net/post/"+str(post.year)+"/"+str(post.month)+"/"+str(post.day)
-        postList.append({'post':{'title':post.title, 'text':post.content, 'date':date, 'url':url}})
-    return json.dumps(postList)
-
-# Create a new post
-def newPost(title, text, day, month, year):
-    post_date = time.mktime((datetime.date(int(year), int(month), int(day))).timetuple())
-    id = int(math.floor(time.time()))    
-    post = Post(id,title,text,post_date,day,month,year)
-    sess.add(post)
-    sess.commit()
-
-# Internally used to return a given post by ID
-def getPostById(id):
-    return sess.query(Post).filter_by(id=id).first()
-
-# Edit a currently-existing post with new data
-def updatePost(id, title, text, day, month, year):
-    post = getPostById(id)
-    post.title = title
-    post.content = text
-    post.day = day
-    post.month = month
-    post.year = year
-    post.date = time.mktime((datetime.date(int(year), int(month), int(day))).timetuple())
-    sess.commit()
-
-# Delete a currently-existing post
-def deletePost(id):
-    post = getPostById(id)
-    sess.delete(post)
-    sess.commit()
